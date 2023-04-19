13d125ad8679a1f804e2c1dd13eca44aa5db8bc6
Bruno Horta <bruno.horta@avanceesoftware.com>
2023-04-19 12:46:32 +0100

    config reset pin

--- src/BoschBHy2Interface.cpp
+++ src/BoschBHy2Interface.cpp
@@ -254,15 +254,15 @@ namespace Motion
         gpio_config_t io_conf = {};
         io_conf.intr_type = GPIO_INTR_DISABLE;
         io_conf.mode = GPIO_MODE_OUTPUT;
-        io_conf.pin_bit_mask = (1ULL << GPIO_NUM_35);
+        io_conf.pin_bit_mask = (1ULL << CONFIG_BHI260AP_RESET);
         io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
         io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
         gpio_config(&io_conf);
-        gpio_set_level(GPIO_NUM_35, 0);
+        gpio_set_level(CONFIG_BHI260AP_RESET, 0);
 
         vTaskDelay(pdMS_TO_TICKS(10));
 
-        gpio_set_level(GPIO_NUM_35, 1);
+        gpio_set_level(CONFIG_BHI260AP_RESET, 1);
     }
 
     esp_err_t initBHy2(Motion::BHI260APSensor *motionSensor)
